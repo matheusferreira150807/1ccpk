5deb235aeb7f4bc868dda352a6221d7805821f39
matheusferreira150807 <mathifutferreira10@gmail.com>
2026-03-16 09:53:25 -0300

    aula 02- variaveis

---
+++ aula02/aula02a-variaveis.py
@@ -0,0 +1,27 @@
+print('olá, mundo')
+print (7+4)
+
+
+nome = "matheus"
+idade = 17
+namorada = "giulia"
+
+print (nome, idade, namorada)
+
+nome = input("digite seu nome")
+idade = input("digite sua idade")
+namorada = input("digite sua namorada")
+
+
+nome = ("giulia")
+
+nome = input("Digite seu nome")
+print("Seja bem vindo, ", nome)
+
+dia = input('digite o dia do seu nascimento')
+mes = input ("digite o mes do seu nascimento")
+ano = input ("digite o ano do seu nascimento")
+
+print (dia, mes, ano)
+
+print (5+5)
